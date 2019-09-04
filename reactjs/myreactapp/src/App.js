7b12dd9fdb562cc31984d513fa70284b530071f2
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-09-04 16:01:13 -0700

    added

---
+++ reactjs/myreactapp/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+     
+	 <button onClick={this.props.onClick}>this button has been clicked{this.props.clicks}times.</button>);
+	 
+	
+	}
+}
+
+export default App;
